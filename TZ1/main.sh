d9ea964fafc6181e29b1d226a2151196a303658b
altf4ronin <167405357+altf4ronin@users.noreply.github.com>
2024-05-01 12:38:27 +0300

    Update main.sh

--- TZ1/main.sh
+++ TZ1/main.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
+# задаем переменные для исходной и выходной директорий
 in_dir=$1
 out_dir=$2
 
 mkdir -p "$out_dir"
-
+# функция для копирования файлов
 copy_file() {
     local in_file=$1
     local out_file=$2
-    local counter=1
-    while [[ -f "$out_file" ]]; do
-        out_file="${2}_$counter"
+    local counter=1 # если номера файлов совпадают, то добавляем номер, который зависит от счетчика
+    while [[ -f "$out_file" ]]; do # проверяем существует ли файл с таким же именем 
+        out_file="${2}_$counter" #добавляем счетчик к имени файла
         ((counter++))
     done
-    cp "$in_file" "$out_file"
+    cp "$in_file" "$out_file" #копирование файла
 }
 
 echo "Файлы, которые находятся вне папок входной директории:"
-find "$in_dir" -maxdepth 1 -type f -exec basename {} \;
+find "$in_dir" -maxdepth 1 -type f -exec basename {} \; 
 
 echo "Папки, находящиеся во входной директории:"
 find "$in_dir" -mindepth 1 -maxdepth 1 -type d -exec basename {} \;
 
 find "$in_dir" -type f -print0 | while IFS= read -r -d $'\0' file; do
-    filename=$(basename -- "$file")
-    copy_file "$file" "$out_dir/$filename"
+    filename=$(basename -- "$file") #находи файл
+    copy_file "$file" "$out_dir/$filename" #копируем в директорию с помощью функции
 done
 
 echo "Все файлы в выходной директории:"
